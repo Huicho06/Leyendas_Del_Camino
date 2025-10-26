43dd5e5c051f5e673fd82c6a10b7be1e6b8d9a55
Mystbeast1234263 <151957407+Mystbeast1234263@users.noreply.github.com>
2025-10-26 03:44:32 -0400

    anim karikari

--- Assets/Scripts/Player/PlayerCameraController.cs
+++ Assets/Scripts/Player/PlayerCameraController.cs
@@ -4,6 +4,7 @@ public class PlayerCameraController : MonoBehaviour
 {
     public float sensibilidad = 200f;
     public Transform playerRoot; // apunta a PlayerRoot
+    public bool blockInput = false;
 
     private float rotacionX = 0f;
 
@@ -14,15 +15,16 @@ public class PlayerCameraController : MonoBehaviour
 
     void Update()
     {
+        if (blockInput) return; // bloquea el input
+
         float mouseX = Input.GetAxis("Mouse X") * sensibilidad * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensibilidad * Time.deltaTime;
 
-        // Rotación vertical de la cámara
         rotacionX -= mouseY;
         rotacionX = Mathf.Clamp(rotacionX, -90f, 90f);
         transform.localRotation = Quaternion.Euler(rotacionX, 0f, 0f);
 
-        // Rotación horizontal del pivot
         playerRoot.Rotate(Vector3.up * mouseX);
     }
+
 }
